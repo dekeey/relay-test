6b1b4cc28bb8fed062957656e1be518f9ed95e66
Dekeey <dekeeytp@gmail.com>
2016-08-27 20:49:18 +0300

    graphql custom type

--- data/schema.js
+++ data/schema.js
@@ -2,22 +2,33 @@ import {
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLInt,
-    GraphQLString
+    GraphQLString,
+    GraphQLList
 } from 'graphql';
 
-let counter =42;
+let data =[
+    { counter: 41 },
+    { counter: 43 },
+    { counter: 42 }
+];
+
+let counterType = new GraphQLObjectType({
+    name: 'Counter',
+    fields: () => ({
+        counter: {
+            type: GraphQLInt
+        }
+    })
+});
 
 let schema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
         fields: () => ({
-            counter: {
-                type: GraphQLInt,
-                resolve: () => counter
-            },
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello from graphql'
+            data: {
+                description: 'data data',
+                type: new GraphQLList(counterType),
+                resolve: () => data
             }
         })
     }),
